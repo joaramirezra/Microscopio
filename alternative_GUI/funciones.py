1987d1d55b7c10bcaee805837b25639669f1dcb3
johan <joaramirezra@unal.edu.co>
2020-09-09 19:31:27 -0500

    v0.005 interfaz optimizada

---
+++ alternative_GUI/funciones.py
@@ -0,0 +1,24 @@
+def turn_on():
+	print('Encendio')
+
+def turn_off():
+	print('apagado')
+
+def home():
+	print('home')
+
+def next_element():
+	print('home')
+
+#     self.home_button.cliked.connect(self.action(3))
+
+    # def action(self,action):
+    #     if (action == 1):
+    #         turn_on()
+    #     else if (action == 2):
+    #         turn_off()
+    #     else if (action == 3):
+    #         home()
+    #     else if (action == 4):
+    #         next()
+    #     else :
---
+++ alternative_GUI/main.py
@@ -0,0 +1,12 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from GUI import *
+from funciones import *
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
