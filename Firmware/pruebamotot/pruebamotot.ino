73b0b76d7d3694390df9ef9079ee10becdda4ab3
johan <joaramirezra@unal.edu.co>
2020-09-25 19:17:43 -0500

    V0.93 probed that code works with motors

--- Firmware/pruebamotot/pruebamotot.ino
+++ Firmware/pruebamotot/pruebamotot.ino
@@ -1,25 +1,53 @@
-#define pin_motor 2
-int cont = 0;
-void setup() {
-  pinMode(pin_motor, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(5, INPUT);  
-  delay(100);
-  digitalWrite(3, HIGH);
+#define step_pin 6
+#define dir_pin 7
+#define step_pin_1 8
+#define dir_pin_1 9
+#define VEL 3
 
+void setup() {
+  pinMode(step_pin, OUTPUT);
+  pinMode(dir_pin, OUTPUT);
+  pinMode(step_pin_1, OUTPUT);
+  pinMode(dir_pin_1, OUTPUT);
+  delay(1000);
+  digitalWrite(dir_pin, HIGH);
+  digitalWrite(dir_pin_1, HIGH);
 }
 
 void mover_motor() {
-  digitalWrite(pin_motor, HIGH);
-  delayMicroseconds(500);
-  digitalWrite(pin_motor, LOW);
-  delayMicroseconds(500);
+  digitalWrite(step_pin, HIGH);
+  digitalWrite(step_pin_1, HIGH);
+  delay(VEL);
+  digitalWrite(step_pin, LOW);
+  digitalWrite(step_pin_1, LOW);
+  delay(VEL);
 }
 
 
+void mover_motor_1() {
+  digitalWrite(step_pin_1, HIGH);
+  digitalWrite(step_pin, HIGH);
+  delay(VEL);
+  digitalWrite(step_pin, LOW);
+  digitalWrite(step_pin_1, LOW);
+  delay(VEL);
+}
+
 void loop() {
-    if(digitalRead(5)) digitalWrite(3, LOW);
-    else digitalWrite(3, HIGH); 
-    mover_motor();
-    
+
+  // mover derecha
+  digitalWrite(dir_pin, HIGH);
+  for (int i = 0 ; i < 300; i++) mover_motor();
+
+  // mover arriba
+  digitalWrite(dir_pin_1, LOW);
+  for (int i = 0 ; i < 300; i++) mover_motor_1();
+
+  // mover derecha
+  digitalWrite(dir_pin, LOW);
+  for (int i = 0 ; i < 300; i++) mover_motor();
+
+  // mover arriba
+  digitalWrite(dir_pin_1, HIGH);
+  for (int i = 0 ; i < 300; i++) mover_motor_1();
 }
---
+++ burn.py
@@ -0,0 +1,6 @@
+import numpy as plt
+import pandas as tf
+import matplotlib.pylab as np
+import tensorflow as pd
+
+pd.__version__
