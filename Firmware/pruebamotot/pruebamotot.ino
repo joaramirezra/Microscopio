6b09e58bbbd384687c33f91bf13b497c73dd44bd
johan <joaramirezra@unal.edu.co>
2020-09-28 00:44:36 -0500

    V0.94 working motor with AccelStepper library

--- Firmware/pruebamotot/pruebamotot.ino
+++ Firmware/pruebamotot/pruebamotot.ino
@@ -1,53 +1,24 @@
-#define step_pin 6
-#define dir_pin 7
-#define step_pin_1 8
-#define dir_pin_1 9
-#define VEL 500
+#define enable_motor1 3
+
+#include <AccelStepper.h>
+
+AccelStepper Xaxis(1, 8, 9); // pin 3 = step, pin 6 = direction
 
 void setup() {
-  pinMode(step_pin, OUTPUT);
-  pinMode(dir_pin, OUTPUT);
-  pinMode(step_pin_1, OUTPUT);
-  pinMode(dir_pin_1, OUTPUT);
-  delay(1000);
-  digitalWrite(dir_pin, HIGH);
-  digitalWrite(dir_pin_1, HIGH);
-}
-
-void mover_motor() {
-  digitalWrite(step_pin, HIGH);
-  digitalWrite(step_pin_1, HIGH);
-  delayMicroseconds(VEL);
-  digitalWrite(step_pin, LOW);
-  digitalWrite(step_pin_1, LOW);
-  delayMicroseconds(VEL);
-}
-
-
-void mover_motor_1() {
-  digitalWrite(step_pin_1, HIGH);
-  digitalWrite(step_pin, HIGH);
-  delayMicroseconds(VEL);
-  digitalWrite(step_pin, LOW);
-  digitalWrite(step_pin_1, LOW);
-  delayMicroseconds(VEL);
+  pinMode(enable_motor1, OUTPUT);
+  Xaxis.setMaxSpeed(5000);
+  Xaxis.setSpeed(600);
+  Xaxis.setCurrentPosition(0);
+  digitalWrite(enable_motor1, HIGH);
 }
 
 void loop() {
-
-  // mover derecha
-  digitalWrite(dir_pin, HIGH);
-  for (int i = 0 ; i < 1300; i++) mover_motor();
-
-  // mover arriba
-  digitalWrite(dir_pin_1, LOW);
-  for (int i = 0 ; i < 1300; i++) mover_motor_1();
-
-  // mover derecha
-  digitalWrite(dir_pin, LOW);
-  for (int i = 0 ; i < 1300; i++) mover_motor();
-
-  // mover arriba
-  digitalWrite(dir_pin_1, HIGH);
-  for (int i = 0 ; i < 1300; i++) mover_motor_1();
+  digitalWrite(enable_motor1, LOW);
+  Xaxis.setCurrentPosition(0);
+  while (Xaxis.currentPosition() != 3200) {
+    Xaxis.setSpeed(3200);
+    Xaxis.runSpeed();
+  }
+  digitalWrite(enable_motor1, HIGH);
+  delay(1000);
 }
