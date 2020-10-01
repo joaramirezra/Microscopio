889c4185cb23a47274ec1e8bbfd0b070650f856a
johan <joaramirezra@unal.edu.co>
2020-09-30 22:05:47 -0500

    V0.94 just some ajustments

--- Firmware/pruebamotot/pruebamotot.ino
+++ Firmware/pruebamotot/pruebamotot.ino
@@ -9,14 +9,16 @@
 AccelStepper Xaxis(1, 8, 9); // pin 3 = step, pin 6 = direction
 
 int Distance_to_Steps (int distance) { //Di
-  int Steps_per_milimeters = (pasos_rev*microPasos/5)*4;
-  return pasos_Calculados*distance;
+  int Steps_per_milimeters = (pasos_rev * microPasos / 5) * 4;
+  return Steps_per_milimeters * distance;
 }
 
 void mover_motor(int pasos, AccelStepper motor) {
   motor.setCurrentPosition(0);
+  if (pasos > 0) motor.setSpeed(1800);
+  else  motor.setSpeed(-1800);
+
   while (motor.currentPosition() != pasos) {
-    motor.setSpeed(800);
     motor.runSpeed();
   }
 }
@@ -30,13 +32,32 @@ void init_motores() {
   digitalWrite(enable_motor1, HIGH);
 }
 
+void move_motor(int distance, int motor_enable , int motor) {
+  digitalWrite(motor_enable, LOW);
+  if (motor == 1 ) mover_motor(Distance_to_Steps(distance), Xaxis);
+  else delay(1);
+  digitalWrite(motor_enable, HIGH);
+}
+
+void go_to_Home() {
+  int cont = 5;
+  while (cont > 0) { // this condition must be replaced by limit swich
+    move_motor(1, enable_motor1, 1);
+    cont--;
+  }
+  delay(1000);
+  cont = 5;
+  while (cont > 0) {
+    move_motor(-1, enable_motor1, 1);
+    cont--;
+  }
+  delay(1000);  
+
+}
 void setup() {
   init_motores();
 }
 
 void loop() {
-  digitalWrite(enable_motor1, LOW);
-  mover_motor(2560, Xaxis);
-  digitalWrite(enable_motor1, HIGH);
-  delay(1000);
+  go_to_Home();
 }
